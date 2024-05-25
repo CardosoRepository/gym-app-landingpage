1b2e7a935cbba5f553d5ef86e8184dd36fc14061
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-05-25 19:51:47 -0300

    Svelte configuration for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,23 @@
-import adapter from "@sveltejs/adapter-netlify";
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-    kit: {
-        target: "#svelte",
-        adapter: adapter(),
-    },
-};
-export default config;
-
-// import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-netlify";
 // import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
 // /** @type {import('@sveltejs/kit').Config} */
 // const config = {
 //     kit: {
+//         target: "#svelte",
 //         adapter: adapter(),
 //     },
 //     preprocess: vitePreprocess(),
 // };
 // export default config;
+
+import adapter from "@sveltejs/adapter-netlify";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    kit: {
+        adapter: adapter(),
+    },
+    preprocess: vitePreprocess(),
+};
+export default config;
